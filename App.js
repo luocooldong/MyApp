2da58b649fb25979da8e1e54be68a5b0882ddaed
dong <gengyudonga@gmail.com>
2019-05-16 20:33:25 +0800

    init

--- App.js
+++ App.js
@@ -1,29 +1,43 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import React from 'react'
+import { View, Text } from 'react-native'
+import { createStackNavigator, createAppContainer } from 'react-navigation'
+import AuthScreen from './src/screens/Auth/Auth'
+import SideDrawer from './src/screens/SideDrawer/SideDrawer'
 
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen</Text>
-      </View>
-    );
+// class HomeScreen extends React.Component {
+//   render() {
+//     return (
+//       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//         <Text>Home Screen</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// class DetailsScreen extends React.Component {
+//   render() {
+//     return (
+//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//         <Text>Details Screen</Text>
+//       </View>
+//     )
+//   }
+// }
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: AuthScreen,
+    SideDrawer: SideDrawer
+  },
+  {
+    initialRouteName: 'Home'
   }
-}
+)
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen
-  }
-});
-
-const AppContainer = createAppContainer(AppNavigator);
+const AppContainer = createAppContainer(AppNavigator)
 
 export default class App extends React.Component {
   render() {
-    return <AppContainer />;
+    return <AppContainer />
   }
 }
-
-
